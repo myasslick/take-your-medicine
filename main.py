6e1ffae37c776d18dcd81a6759635529d9eed892
Yeuk Hon Wong <yeukhon@acm.org>
2017-12-10 20:34:46 -0500

    I can now send message to my cell phone...

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import datetime
 import os
 
 from twilio.rest import Client
 
+CHECKIN_MSG_TEMPLATE = (
+    '{date} ({day})\n' +
+    'Please confirm you have taken your medicine today (Yes/No).'
+)
+
+def make_checkin_message():
+    today = datetime.datetime.today()
+    weekday = today.strftime("%a")
+    date = "%s/%s" %(today.month, today.day)
+
+    return CHECKIN_MSG_TEMPLATE.format(date=date,
+        day=weekday)
+
 if __name__ == "__main__":
     account = os.environ["TWILIO_ACCOUNT"]
-    token = os.environ["TWILIO_TOKEN"]
-    sender_number = os.environ["TWILIO_SENDER_PHONE_NUMBER"]
-    receiver_number = os.environ["TWILIO_RECIVER_PHONE_NUMBER"]
+    token = os.environ["TWILIO_AUTH_TOKEN"]
+    twilio_number = os.environ["TWILIO_PHONE_NUMBER"]
+    home_number = os.environ["TWILIO_HOME_PHONE_NUMBER"]
 
-    client = Client()
+    client = Client(account, token)
+    checkin_message = make_checkin_message()
+
+    message = client.messages.create(from_=twilio_number,
+        to=home_number, body=checkin_message)
